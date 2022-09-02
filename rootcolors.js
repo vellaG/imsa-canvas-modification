3a8c9993d6d778ab20566de8dce4b5a42f7f6642
vellaG <jeremiahsuarez7267@gmail.com>
2022-09-01 19:26:29 -0500

    initial commit

---
+++ rootcolors.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',()=>{
+    var x = document.createElement('link');
+    x.rel = "stylesheet"
+    x.href =  chrome.runtime.getURL("css/rootcolors.css")
+    document.head.appendChild(x)
+})
