a9f4a43629142c1fee70012c1212b33126e4c491
vellaG <jeremiahsuarez7267@gmail.com>
2022-09-01 22:51:45 -0500

    almost done i think

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,7 @@
 
-window.addEventListener('load',()=>{
-    var x = document.createElement('link');
-    x.rel = "stylesheet"
-    x.href =  chrome.runtime.getURL("css/dashboard.css")
-    document.head.appendChild(x)
 
 
-    //remove logo bc its not a transparent background wtf
-    let firstlogos =  document.getElementsByClassName("ic-sidebar-logo")
-    let first = firstlogos[0]
-    console.log(first)
-})
+var x = document.createElement('link');
+x.rel = "stylesheet"
+x.href =  chrome.runtime.getURL("css/dashboard.css")
+document.head.appendChild(x)
--- rootcolors.js
+++ rootcolors.js
@@ -1,6 +1,6 @@
-window.addEventListener('load',()=>{
-    var x = document.createElement('link');
-    x.rel = "stylesheet"
-    x.href =  chrome.runtime.getURL("css/rootcolors.css")
-    document.head.appendChild(x)
-})
+
+
+var x = document.createElement('link');
+x.rel = "stylesheet"
+x.href =  chrome.runtime.getURL("css/rootcolors.css")
+document.head.appendChild(x)
